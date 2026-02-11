536eb62f68bb1ff4a10af59d6a476ccda01a09db
aaron.tralongo <aaron.tralongo@saintandrews.net>
2026-02-11 08:26:09 -0500

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("com.google.api-client:google-api-client:2.0.0")
+    implementation("com.google.oauth-client:google-oauth-client-jetty:1.34.1")
+    implementation("com.google.apis:google-api-services-docs:v1-rev20220609-2.0.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
