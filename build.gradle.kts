1785c7ca1ec1912ac550be8a75919d0ac2d99780
aaron.tralongo <aaron.tralongo@saintandrews.net>
2026-02-17 13:22:15 -0500

    out of class commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,11 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("com.google.api-client:google-api-client:2.0.0")
     implementation("com.google.oauth-client:google-oauth-client-jetty:1.34.1")
+
     implementation("com.google.apis:google-api-services-docs:v1-rev20220609-2.0.0")
+    implementation("com.google.apis:google-api-services-sheets:v4-rev20220927-2.0.0")
+    implementation("com.google.apis:google-api-services-calendar:v3-rev20220715-2.0.0")
+    implementation("com.google.apis:google-api-services-gmail:v1-rev20220404-2.0.0")
 }
 
 tasks.test {
